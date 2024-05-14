2235a82cefd844960ea55f0859c9f0a11a3d7de2
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-14 17:23:33 +0100

    add gitignore + app to return welcome page

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+basic flask app with single route to hello world page
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """index function returning a rendered html page"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
